3d693934bab4218640ab4b4160640d80f6fcd66e
anilachundi <anilachundi@gmail.com>
2025-01-12 15:33:02 -0800

    finished app version1

---
+++ frontend/src/components/Table.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import './table.css'
+
+const Table = ({data, column}) => {
+    
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {column.map((item, index) => <TableHeadItem item={item} />)}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, index) => <TableRow item={item} column={column}/>)}
+            </tbody>
+
+        </table>
+
+    );
+}
+
+
+
+
+const TableHeadItem = ({ item }) => <th>{item.heading}</th>
+const TableRow = ({ item, column }) => (
+    <tr>
+      {column.map((columnItem, index) => {
+  
+        
+  
+        return <td>{item[`${columnItem.value}`]}</td>
+      })}
+    </tr>
+  )
+
+  export default Table;
